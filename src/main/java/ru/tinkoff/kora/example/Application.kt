5ac2dc5d2878c46e048e422b2a804bbd7b95f9e2
Anton Kurako <goodforgod.dev@gmail.com>
2025-10-01 14:12:23 +0300

    Cleanup

--- src/main/java/ru/tinkoff/kora/example/Application.kt
+++ src/main/java/ru/tinkoff/kora/example/Application.kt
@@ -5,7 +5,6 @@ import ru.tinkoff.kora.common.KoraApp
 import ru.tinkoff.kora.config.hocon.HoconConfigModule
 import ru.tinkoff.kora.http.server.undertow.UndertowModule
 import ru.tinkoff.kora.logging.logback.LogbackModule
-import ru.tinkoff.kora.micrometer.module.MetricsModule
 
 
 @KoraApp
